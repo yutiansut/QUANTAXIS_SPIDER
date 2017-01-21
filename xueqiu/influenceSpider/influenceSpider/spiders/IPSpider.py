f6e330c6f060e60b77f22e39b3697dece3ecedf3
yutiansut <yutiansut@qq.com>
2017-01-21 23:23:53 +0800

    login

--- xueqiu/influenceSpider/influenceSpider/spiders/IPSpider.py
+++ xueqiu/influenceSpider/influenceSpider/spiders/IPSpider.py
@@ -5,25 +5,50 @@ from scrapy.http import Request
 from scrapy.selector import Selector  
 from influenceSpider.items import InfluencespiderItem
 import json
+import base64
+import requests
+import cookies
 
-with open('setting.json', 'r') as f:
- user = json.load(f)
- 
+with open('influenceSpider/spiders/setting.json', 'r') as f:
+    user = json.load(f)
+    user.mail = user['mail']
+    user.telephone=user['phone']
+    user.psw = user['psw']
+    #
+
 class IpspiderSpider(scrapy.Spider):
     name = "IPSpider"
     allowed_domains = ["xueqiu.com"]
     start_urls = ['https://xueqiu.com/user/login']
     item = [ ]
-
+    cookies = []
     def login(self,response):
-        cookies = 1;
-        user=[];
+        loginurls="https://xueqiu.com/user/login"
+        formdata= {
+            "areacode":"86",
+            "password":base64.b64encode(user.psw.encode('utf-8')).decode('utf-8'),
+            "remember_me":"on",
+            "telephone":user.telephone
+        }
+        session = requests.Session()
+        r = session.post(loginurls, data=formdata)
+        jsonStr = r.content.decode('gbk')
+        info = json.loads(jsonStr)
+        if info["retcode"] == "0":
+            print "Get Cookie Success!( Account:%s )" % user.telephone
+            cookie = session.cookies.get_dict()
+            cookies.append(cookie)
+        else:
+            print "Failed!( Reason:%s )" % info['reason']
+
+    print cookies
+    return cookies
 
 
 
 
 
-        return cookies
+        
     def parse(self, response):
         sel = Selector(response)  
         item=InfluencespiderItem();
