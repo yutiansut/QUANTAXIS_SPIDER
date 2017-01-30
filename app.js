df36bb5f0d3b4eede9d60404884c1df941ed047d
yutiansut <yutiansut@qq.com>
2017-01-30 17:42:43 +0800

    commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var cors = require('cors')
 var index = require('./routes/index');
 var users = require('./routes/users');
+var apis = require('./routes/api');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/apis', apis);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
