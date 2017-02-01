7a9b6fc3fd719c81c46588dd3d28b1770f857471
yutiansut <yutiansut@qq.com>
2017-02-01 10:45:19 +0800

    后端api打包 restful

--- backend/routes/apis.js
+++ backend/routes/apis.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var wscQuery =require('../database/config')
+require('mongoose');
+var mongodb=require('mongodb');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -11,18 +13,41 @@ router.get('/json', function(req, res, next) {
 });
 
 
-router.get('/querybyname', function(req, res, next) {
-    console.log('get data')
-    console.log(req.query.name)
-    wscQuery.find({content:req.query.name},function(err,docs){
-        if(!err){
-            console.log(docs);
-        	res.json(docs);
-        }else{
-            console.log(err);
-        }
-    });
+router.get('/queryContentbyName', function(req, res, next) {
+    console.log('get data');
+    console.log(req.query.name);
+
+     if(req.query.name) {
+    var name=new RegExp(req.query.name);//模糊查询参数
+        console.log(name);
+    }
+    mongodb.connect('mongodb://localhost:27017/wsc', function(err, conn){
+         conn.collection('articles', function(err, coll){ 
+              coll.find({'content':name}).toArray(function(err,docs){
+                  console.log(docs)
+                  res.json(docs)
+         })
+        })
+    })
 });
+router.get('/queryTitlebyName', function(req, res, next) {
+    console.log('get data');
+    console.log(req.query.name);
+
+     if(req.query.name) {
+    var name=new RegExp(req.query.name);//模糊查询参数
+        console.log(name);
+    }
+    mongodb.connect('mongodb://localhost:27017/wsc', function(err, conn){
+         conn.collection('articles', function(err, coll){ 
+              coll.find({'title':name}).toArray(function(err,docs){
+                  console.log(docs)
+                  res.json(docs)
+         })
+        })
+    })
+});
+
 
 
 module.exports = router;
--- readme.md
+++ readme.md
@@ -12,7 +12,15 @@ python-scrapy-selenium-webdriver
 mongodb
 redis
 ```
+## 技术栈
 ![技术栈](https://github.com/yutiansut/QUANTAXIS_SPIDER/blob/dev-visualCraw/todo.png)
+## 爬取到的数据
 ![爬到的数据](https://github.com/yutiansut/QUANTAXIS_SPIDER/blob/dev-front-back-Craw/pic/craw.png)
+## 数据库查重
 ![数据库查重](https://github.com/yutiansut/QUANTAXIS_SPIDER/blob/dev-front-back-Craw/pic/findsame.png)
+## 从数据库索引中爬取
 ![从数据库文章索引爬取](https://github.com/yutiansut/QUANTAXIS_SPIDER/blob/dev-front-back-Craw/pic/getfromdatabase.png)
+## 后端api做restful
+![后端api做restful](https://github.com/yutiansut/QUANTAXIS_SPIDER/blob/dev-front-back-Craw/pic/backend-restful.png)
+## 前端拿到api
+![前端拿到api](https://github.com/yutiansut/QUANTAXIS_SPIDER/blob/dev-front-back-Craw/pic/front-getapi.png)
