df56e82cd127745837539af5d3722be8cbb382d8
yutiansut <yutiansut@qq.com>
2017-02-02 17:19:22 +0800

    前端更新

---
+++ frontend/quantaxisSpider/src/components/Bar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="bar">
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>Center</li>
+            <li>About</li>
+        </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    name: 'bar'
+}
+</script>
