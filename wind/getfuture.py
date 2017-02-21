2973db83b51160faf69d7c6c94f429c4ea9219ff
yutiansut <yutiansut@qq.com>
2017-02-21 21:22:13 +0800

    commit

--- wind/getfuture.py
+++ wind/getfuture.py
@@ -1,5 +1,13 @@
 # -*- coding:utf-8 -*-
-
-
+from WindPy import *
+w.start()   
+from datetime import datetime
+from pymongo import MongoClient  
+import talib
 #先搞上期
-w.wset("SectorConstituent","date=20170221;sectorId=a599010201000000")
+#数据库
+client = MongoClient('localhost', 27017)  
+db = client.wind
+coll = db.future
+
+
