74f070cb338aaa8172771fae616c1b804ca9d630
yutiansut <yutiansut@qq.com>
2017-02-21 19:56:29 +0800

    commit

---
+++ futures/getfuture.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+from WindPy import *
+w.start()
+from datetime import datetime
+
+
+#先搞上期
+w.wset("SectorConstituent","date=20170221;sectorId=a599010201000000")
--- spiders/nodejsSpider/exp/1.js
+++ spiders/nodejsSpider/exp/1.js
@@ -5,14 +5,18 @@
 var cheerio = require('cheerio');
 var request = require('superagent-charset');
 
+var ggtj =exports.ggtj =function(){
+     var testx=request
+                .get('http://vip.stock.finance.sina.com.cn/q/go.php/vLHBData/kind/ggtj/index.phtml')
+                .charset('gbk')
+                .end(function(err,res){
+                    var $ = cheerio.load(res.text);
+                    var test= $('#dataTable').text();
+                    return this.test
+                })
+    console.log(testx)
 
-request
-.get('http://vip.stock.finance.sina.com.cn/q/go.php/vLHBData/kind/ggtj/index.phtml')
-.charset('gbk')
-.end(function(err,res){
-    var $ = cheerio.load(res.text);
-    console.log($('#dataTable').text())
-})
+}
 
 //result
 /**
