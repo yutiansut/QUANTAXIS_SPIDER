bc3d88f4641d6683fd811ef6962d9abe2d8b2666
yutiansut <yutiansut@qq.com>
2017-03-30 13:33:51 +0800

    ss

--- QUANTAXIS_Spiders/QUANTAXIS_Spiders/settings.py
+++ QUANTAXIS_Spiders/QUANTAXIS_Spiders/settings.py
@@ -16,13 +16,13 @@ NEWSPIDER_MODULE = 'QUANTAXIS_Spiders.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'QUANTAXIS_Spiders (+http://www.yourdomain.com)'
+USER_AGENT = 'QUANTAXIS'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
